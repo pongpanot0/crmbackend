6860b96afa06b9ff8d4b7f2e735ed702f42f12f6
Pongpanot Samarkkarn <61924591+pongpanot0@users.noreply.github.com>
2022-09-18 22:03:09 +0700

    1

--- controller/product.js
+++ controller/product.js
@@ -1,5 +1,5 @@
 const db = require("../db/db");
-
+const moment = require("moment");
 exports.createproduct = async (req, res) => {
   let product_type = req.body.product_type;
   let product_name = req.body.product_name;
@@ -10,16 +10,40 @@ exports.createproduct = async (req, res) => {
   let product_amount = req.body.product_amount;
   let supplies_id = req.body.supplies_id;
   let created_by = req.body.created_by;
-  let created_at = req.body.created_at;
+  let created_at = moment(new Date()).format("DD-MM-YYYY");
   let updated_by = req.body.updated_by;
-  let updated_at = req.body.updated_at;
-  console.log(req.body)
+  let updated_at = moment(new Date()).format("DD-MM-YYYY");
   const create = `insert into product (product_type,product_name,product_price,product_cost,company_id,product_group,product_amount,supplies_id,created_by,created_at,updated_by,updated_at) values('${product_type}','${product_name}','${product_price}','${product_cost}','${company_id}','${product_group}','${product_amount}','${supplies_id}','${created_by}','${created_at}','${updated_by}','${updated_at}')`;
   db.query(create, (err, result) => {
     if (err) {
       console.log(err);
     }
     if (result) {
+      res.send({
+        status: 200,
+        data: result,
+      });
+    }
+  });
+};
+exports.editproduct = async (req, res) => {
+  const id = req.params.id;
+  let product_type = req.body.product_type;
+  let product_name = req.body.product_name;
+  let product_price = req.body.product_price;
+  let product_cost = req.body.product_cost;
+  let product_group = req.body.product_group;
+  let product_amount = req.body.product_amount;
+  let supplies_id = req.body.supplies_id;
+  let updated_by = req.body.updated_by;
+  let updated_at = moment(new Date()).format("DD-MM-YYYY");
+  const create = `update  product  set product_type='${product_type}',product_name='${product_name}',product_price='${product_price}',product_cost='${product_cost}',product_group='${product_group}',product_amount='${product_amount}',supplies_id='${supplies_id}',updated_by='${updated_by}',updated_at='${updated_at}' where product_id = ${id}`;
+  db.query(create, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    if (result) {
+      console.log(result)
       res.send({
         status: 200,
         data: result,
@@ -47,7 +71,21 @@ exports.getproduct = async (req, res) => {
     }
   });
 };
+exports.getproductid = async (req, res) => {
+  let id = req.params.id;
+  let create = `select * from product where product_id = ${id}`;
 
+  db.query(create, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    if (result) {
+      res.send({
+        data: result,
+      });
+    }
+  });
+};
 exports.getproductbySupplier = async (req, res) => {
   let id = req.params.id;
   let create = `select * from  product where supplies_id = ${id}`;
--- routes/product.js
+++ routes/product.js
@@ -2,4 +2,7 @@ const product = require("../controller/product");
 module.exports = function (app) {
   app.post("/product/create", product.createproduct);
   app.get("/product/getall/:id", product.getproduct);
+  app.get("/product/getproductid/:id", product.getproductid);
+  app.post("/product/editproduct/:id", product.editproduct);
+  
 };
